b3d882fbf9a669b7b4b03e31e5c3c62afc5904df
Tane4ka170 <tane4ka170@gmail.com>
2024-05-04 00:43:15 +0300

    started navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import pokeballIcon from "../assets/pokeball-icon.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt="Pokeball Icon" />
+      </div>
+      <div className="data"></div>
+      <div className="block">
+        <GiHamburgerMenu />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/sections/Wrapper.tsx
+++ src/sections/Wrapper.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Wrapper = () => {
-  return <div>Wrapper</div>;
+  return <div className="conent">Wrapper</div>;
 };
 
 export default Wrapper;
